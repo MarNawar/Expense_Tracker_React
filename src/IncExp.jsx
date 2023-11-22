41a5a5e59a48d893c9ec3e43014316ba2ab92f9a
MarNawar <107632957+MarNawar@users.noreply.github.com>
2023-11-22 20:53:05 +0000

    Bug Fixed for Delete Button

--- src/IncExp.jsx
+++ src/IncExp.jsx
@@ -1,35 +1,40 @@
-import {useState, useEffect, useContext} from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { Transaction } from './context/context';
 
-function IncExp(){
-  const {expenseState, dispatch} = useContext(Transaction);
-  
+function IncExp() {
+  const { expenseState, dispatch } = useContext(Transaction);
+
   const [history, setHistory] = useState([]);
-  useEffect(()=>{    
-    setHistory(()=>{
+  useEffect(() => {
+    setHistory(() => {
       return <ul>
-      {
-        expenseState.transactions.map((item) => {
-          if(item.amount>0){
-            return <li className="plus" key={item.id}>
-              {item.text}<span>{item.amount}</span> <button className="delete-btn" onClick={()=>{
-                dispatch({
-                  type: "DELETE_TRANSACTION",
-                  payload: item.id
-                })
-              }}>X</button>
-            </li> 
-          }
-          else{
-            return <li className="minus" key={item.id}>
-              {item.text}<span>{item.amount}</span> 
-            </li>
-          }
-        })
-      }
+        {
+          expenseState.transactions.map((item) => {
+            if (item.amount > 0) {
+              return <li className="plus" key={item.id}>
+                {item.text}<span>{item.amount}</span> <button className="delete-btn" onClick={() => {
+                  dispatch({
+                    type: "DELETE_TRANSACTION",
+                    payload: item.id
+                  })
+                }}>X</button>
+              </li>
+            }
+            else {
+              return <li className="minus" key={item.id}>
+                {item.text}<span>{item.amount}</span> <button className="delete-btn" onClick={() => {
+                  dispatch({
+                    type: "DELETE_TRANSACTION",
+                    payload: item.id
+                  })
+                }}>X</button>
+              </li>
+            }
+          })
+        }
       </ul>
     })
-  },[expenseState]);
+  }, [expenseState]);
 
 
   return (
